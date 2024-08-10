332e5e9807258452bb606edac0910ece608a8deb
hyojinLeee-git <splato8899@gmail.com>
2024-08-10 20:23:41 +0900

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { motion, useMotionValue, useTransform } from "framer-motion";
+
+const ShrinkingCircle: React.FC = () => {
+  const [isDragging, setIsDragging] = useState(false);
+  const x = useMotionValue(0);
+  const size = useTransform(x, [0, 300], [200, 50]);
+
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "400px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <motion.div
+        style={{
+          width: size,
+          height: size,
+          borderRadius: "50%",
+          backgroundColor: "blue",
+          cursor: isDragging ? "grabbing" : "grab",
+        }}
+        drag="x"
+        dragConstraints={{ left: 0, right: 300 }}
+        onDragStart={() => setIsDragging(true)}
+        onDragEnd={() => setIsDragging(false)}
+        onDrag={(_, info) => x.set(info.point.x)}
+      />
+    </div>
+  );
+};
+
+export default ShrinkingCircle;
